cea538038550514717dea4f03600ec763be6ca6a
padi2312 <patrick.arndt.99@web.de>
2024-03-23 20:44:47 +0100

    fix bug where subpages were not corectly rendered after reload + add time measurement

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,26 @@ mod dorcs;
 use std::path::PathBuf;
 
 use crate::dorcs::config::Config;
-use dorcs::sections::SectionGenerator;
 use dorcs::sections::SectionBuilder;
+use dorcs::sections::SectionGenerator;
+use std::time::Instant;
 
 fn main() {
     let config: Config = Config::load();
     let source_dir = config.source;
     let output_dir = config.output;
 
+    let start_time = Instant::now();
+
     let section_constructor = SectionGenerator::new(PathBuf::from(&source_dir));
     let section = section_constructor.construct();
-    println!("Section: {:#?}", section);
 
     let section_handler = SectionBuilder::new(section);
     section_handler.execute(&output_dir);
+
+    let end_time = Instant::now();
+    let elapsed_time = end_time - start_time;
+
+    println!("✅ Build complete in {:?}!", elapsed_time);
+    println!("📄 Your documentation is available in: {}", output_dir);
 }
